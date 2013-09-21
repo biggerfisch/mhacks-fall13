2203b68b184331a716876d3fc16a71fc35b0be41
Charles Lewis <noodle@umich.edu>
2013-09-21 15:27:22 -0400

    Something something routes

--- chords.py
+++ chords.py
@@ -42,10 +42,6 @@ def fetch_song(token):
         return jsonify({'notes': song['notes']})
     else:
         abort(404)
-    
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
 
 @app.route('/about/')
 def about_page():
