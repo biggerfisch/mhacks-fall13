0c40400273cefdad45efb9710991022116183dc4
Shu <shudas@umich.edu>
2013-09-21 08:12:24 -0400

    Saturday morning baka

--- MIDIBridge-master/examples/js/computer-keyboard.js
+++ MIDIBridge-master/examples/js/computer-keyboard.js
@@ -12,6 +12,13 @@
  * 
  */
 
+// My addition:
+var timeDistinction,    // Helps distinguish which metronome beat we are aligned with. should be (60/bpm)*1000/2
+noteOnTime,             // The timestamp when noteOn occurred
+noteOffTime,            // The timestamp when noteOff occurred
+tickTime,               // The timestamp when the metronome tick happened
+messages = document.getElementById("messages");
+
 window.addEventListener('load', function() {
 
     if(midiBridge.userAgent === "msie8/win"){
@@ -29,6 +36,15 @@ window.addEventListener('load', function() {
     selectOutput = document.getElementById("outputs"),
     messageDiv = document.getElementById("help-message");
     
+    window.performance = window.performance || {};
+    performance.now = (function() {
+      return performance.now       ||
+             performance.mozNow    ||
+             performance.msNow     ||
+             performance.oNow      ||
+             performance.webkitNow ||
+             function() { return new Date().getTime(); };
+    })();
         
     function connectKeyboard(){
 
@@ -56,18 +72,21 @@ window.addEventListener('load', function() {
         }
         
         document.addEventListener("keydown", function(e) {
-            //console.log(e, e.which, e.which.toString(), noteNumbers[e.which.toString()]);
+            console.log(e, e.which, e.which.toString(), noteNumbers[e.which.toString()]);
             if(!output){
                 return;
             }
             if(e.which === 32) {//spacebar acts as sustain pedal
                 output.sendMIDIMessage(midiAccess.createMIDIMessage(midiBridge.CONTROL_CHANGE, 1, 64, 127));
             } else if(noteNumbers[e.which] && !keysPressed[e.which]) {
-                output.sendMIDIMessage(midiAccess.createMIDIMessage(midiBridge.NOTE_ON, 1, noteNumbers[e.which], 100));
+                noteOnMessage = midiAccess.createMIDIMessage(midiBridge.NOTE_ON, 1, noteNumbers[e.which], 100);
+                output.sendMIDIMessage(noteOnMessage);
                 keysPressed[e.which] = true;
+                // MY ADDITION
+                noteOnTime = performance.now();
+                messages.innerHTML += noteNumbers[e.which] + "NoteOnTime:" + noteOnTime + "<br/>";
             }
         }, false);
-    
     
         document.addEventListener("keyup", function(e) {
             if(!output){
@@ -76,15 +95,17 @@ window.addEventListener('load', function() {
             if(e.which === 32) {
                 output.sendMIDIMessage(midiAccess.createMIDIMessage(midiBridge.CONTROL_CHANGE, 1, 64, 0));
             } else if(noteNumbers[e.which]) {
-                output.sendMIDIMessage(midiAccess.createMIDIMessage(midiBridge.NOTE_OFF, 1, noteNumbers[e.which], 0));
+                noteOffMessage = midiAccess.createMIDIMessage(midiBridge.NOTE_OFF, 1, noteNumbers[e.which], 0);
+                output.sendMIDIMessage(noteOffMessage);
                 keysPressed[e.which] = false;
+                // MY ADDITION
+                noteOffTime = performance.now();
+                messages.innerHTML += noteNumbers[e.which] + "NoteOffTime:" + noteOffTime + "<br/>";
             }
         }, false);
         
         messageDiv.innerHTML = msgSelectOutput;
     }
-
-
 
     midiBridge.init(function(_midiAccess){
         
@@ -110,5 +131,196 @@ window.addEventListener('load', function() {
         connectKeyboard();
     });
 
+    // GETS DATA REGARDING IMPORTANT PARTS OF MIDI MESSAGE. TIMESTAMP SHOULD BE Performance.now()
+    function getRelevantMidiData(midiMessage, timestamp){
+
+    }
 
 }, false);
+
+// the metronome!!!
+var metronome = function(opts) {
+    //primary variables
+    var l = typeof opts.len !== "undefined" ? opts.len : 200, // length of metronome arm
+        r = typeof opts.angle !== "undefined" ? opts.angle : 20, //max angle from upright 
+        w = 2 * l * Math.cos(r),
+        tick_func = typeof opts.tick !== "undefined" ? opts.tick : function() {}, //function to call with each tick
+        end_func = typeof opts.complete !== "undefined" ? opts.complete : function() {}, //function to call on completion
+        playSound = typeof opts.sound !== "undefined" ? opts.sound : true; 
+
+    // initialize Raphael paper if need be        
+    switch(typeof opts.paper) {
+        case "string": paper = Raphael(opts.paper, w, l + 20); break;
+        case "undefined": paper = Raphael(0, 0, w, l + 20); break;
+    }
+
+    // initialize audio if need be
+    if (playSound && opts.audio) {
+        // initialize audio
+        var sound = document.createElement('audio');
+        sound.setAttribute('src', opts.audio);
+        sound.setAttribute('id', 'tick');
+        document.body.appendChild(sound);
+    }
+    
+    // derivative variables
+    var y0 = l * Math.cos(Math.PI * r / 180),
+        x0 = l * Math.sin(Math.PI * r / 180),    
+        y = l + 10,
+        x = x0 + 10,    
+        tick_count = 0;
+    
+    var outline = paper.path("M"+x+","+y+"l-"+x0+",-"+y0+"a"+l+","+l+" "+2*r+" 0,1 "+2*x0+",0L"+x+","+y).attr({
+        fill: "#EEF",
+        'stroke-width': 0    
+    });
+    
+    var arm = paper.path("M" + x + "," + (y + 5) + "v-" + (l - 5)).attr({
+        'stroke-width': 5,
+        stroke: "#999"
+    }).data("id", "arm");
+        
+    var weight = paper.path("M" + x + "," + (y-100) + "h12l-3,18h-18l-3-18h12").attr({
+        'stroke-width': 0,
+        fill: '#666'
+    }).data("id", "weight");
+
+    var vertex = paper.circle(x, y, 7).attr({
+        'stroke-width': 0,
+        fill: '#CCC'
+    }).data("id", "vertex");
+
+    var label = paper.text(x, y + 20, "").attr({
+        "text-anchor": "center",
+        "font-size": 14
+    });
+
+    var mn = paper.set(arm, weight);
+    
+    Raphael.easing_formulas.sinoid = function(n) { return Math.sin(Math.PI * n / 2) };
+
+    function tick(obj, repeats) {      
+        //Raphael summons the callback on each of the three objects in the set, so we
+        //have to only call the sound once per iteration by associating it with one of the objects.
+        //doesn't matter which one
+        if (obj.data("id") === "arm") {
+            tick_count += 1;
+            // TIMESTAMP WHEN TICK OCCURS
+            tickTime = performance.now();
+            messages.innerHTML += "metronomeTime:" + tickTime + "<br/>";
+            barNum = document.getElementById("barNum");
+            // SHOW BAR NUMBER
+            if (tick_count < 4)
+            {
+                barNum.innerHTML = tick_count;
+            }
+            else if (tick_count % 4 === 0)
+            {
+                barNum.innerHTML = 4;
+            }
+            else
+            {
+                barNum.innerHTML = tick_count % 4;
+            }
+            // CHANGE BAR NUMBER COLOR
+            if (barNum.innerText == "1")
+            {
+                barNum.style.color = "DarkRed";
+            }
+            else
+            {
+                barNum.style.color = "DarkBlue";
+            }
+
+            if (playSound) {    
+                document.getElementById("tick").play();
+            }
+            tick_func(tick_count);            
+            if (tick_count >= repeats) {
+                mn.attr("transform", "R0 " + x + "," + y);    
+                end_func();
+            }    
+        }
+    }    
+
+    return {
+        start: function(tempo, repeats, tickf, donef) {
+            if (tickf) {
+                tick_func = tickf;
+            }
+            if (donef) {
+                end_func = donef;
+            }
+            
+            tick_count = 0;
+            mn.attr("transform", "R-" + r + " " + x + "," + y);                
+            
+            //2 iterations per animation * 60000 ms per minute / tempo
+            var interval = 120000 / tempo;
+
+            var animationDone = function() { 
+                tick(this, repeats); 
+            };
+            
+            var ticktockAnimationParam = {
+                "50%": { transform:"R" + r + " " + x + "," + y, easing: "sinoid", callback: animationDone },
+                "100%": { transform:"R-" + r + " " + x + "," + y, easing: "sinoid", callback: animationDone }
+            };
+            
+            //animation            
+            var ticktock = Raphael.animation(ticktockAnimationParam, interval).repeat(repeats / 2);
+            arm.animate(ticktock);
+            weight.animateWith(arm, ticktockAnimationParam, ticktock); 
+        },
+        stop: function() {
+            // Reset the bar number
+            document.getElementById("barNum").innerHTML = "<br/>"
+            mn.stop();
+            mn.attr("transform", "R0 " + x + "," + y);                
+            end_func();
+        },
+        shapes: function() {
+            return {
+                outline: outline,
+                arm: arm,
+                weight: weight,
+                vertex: vertex          
+            }
+        },
+        make_input: function(el) {
+            $("<div />", {
+                html:   "<span>tempo: </span>" + 
+                        "<input class='metr_input' type='text' id='tempo' value='100' />" +
+                        "<span>ticks: </span>" +
+                        "<input class='metr_input' type='text' id='ticks' value='12000' />" +
+                        "<button id='startstop'>start</button>"
+            }).appendTo(el);
+            
+            $('#startstop').click(function() {
+                // start animation
+                if ($(this).html() === "start") {
+                    $(this).html("stop");            
+                    
+                    //get values for tempo and ticks and restrict
+                    var tempo = parseInt($('#tempo').val(), 10);
+                    if (!tempo) { tempo = 60; }
+                    else if (tempo > 200) { tempo = 200; }
+                    else if (tempo < 30) { tempo = 30; }
+                    $("#tempo").val(tempo);
+
+                    // Set the timedistinction value.
+                    timeDistinction = (60/tempo)*1000/2;
+                    
+                    var ticks = parseInt($('#ticks').val(), 10);
+                    if (!ticks || ticks < 8) { ticks = 12000; }
+                    $("#ticks").val(ticks); 
+                    
+                    m.start(tempo, ticks);
+                } else {
+                    $(this).html("start");
+                    m.stop();
+                }
+            });                         
+        }
+    };
+};
